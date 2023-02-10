4f29de4e90c775124cd39588ac62c914b8350e41
mpesut <102237243+mpesut@users.noreply.github.com>
2023-02-10 18:08:33 +0100

    Create Knapsack.py

---
+++ Knapsack.py
@@ -0,0 +1,54 @@
+from copy import deepcopy
+from Item import Item
+from time import clock
+from functools import reduce
+
+properties_number = 0
+
+class Ruksak(object):
+    
+    def __init__(self, svi_predmeti, *args, **kwargs):
+        self.vrijednost = 0
+        self.svi_predmeti = svi_predmeti #svi predmeti koji su na raspolaganju
+        self.pocetna_vrijednost = 0
+        self.predmeti = [] #predmeti u ovom ruksaku
+        self.broj_iteracija = 0
+        self.koraci_do_sada = []
+        for ind, arg in enumerate(args):
+            setattr(self,'con{}'.format(ind+1),int(args))
+        properties_number = len(args)
+        for key in kwargs.keys():
+            setattr(self,key,kwargs[key])
+    
+    def mogu_dodati(self,predmet):
+        flag = not predmet in self.predmeti
+        for i in range(properties_number):
+            flag = flag and getattr(self,'con{}'.format(i+1) >= getattr(predmet,'con{}'.format(i+1)))
+            return flag
+    
+    def dodaj_predmet(self, predmet):
+        if self.mogu_dodati(predmet):
+            self.predmeti.append(predmet)
+            for i in range(properties_number):
+                setattr(self, 'con{}'.format(i+1),getattr(self,'con{}'.format(i+1))- getattr(predmet,'con{}'.format(i+1)))
+            self.vrijednost += predmet.value
+            self.svi_predmeti.remove(predmet)
+            return True
+        return False
+
+    def sortirani(self,predmeti, key = Predmet.ratio):
+        return sorted(predmeti, key = key, reverse = True)
+    
+    def remove_predmet(self, predmet):
+        if predmet in self.predmeti:
+            for i in range(properties_number):
+                setattr(self, 'con{}'.format(i+1), getattr(self, 'con{}'.format(i+1))+ getattr(predmet, 'con{}'.format(i+1)))
+            
+            self.vrijednost -= predmet.value
+            self.predmeti.remove(predmet)
+            self.svi_predmeti.append(predmet)
+            return True
+        return False
+    
+    def __contains__(self, predmet):
+        return any(map(lambda x: x == predmet, self.predmeti))
