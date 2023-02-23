5fd191a8522d921ec1711550ef3ef1367c8357ee
anazebi <anazebi.math@pmf.hr>
2023-02-23 19:37:15 +0100

    update

--- TabuSearch.py
+++ TabuSearch.py
@@ -1,22 +1,22 @@
 from copy import deepcopy
 from functools import reduce
 
-# tabu lista koja sadrži "zabranjena stanja" 
-# ova klasa nasljeðuje od klase list
+# tabu lista koja sadrzi "zabranjena stanja" 
+# ova klasa nasljeduje od klase list
 class TabuList(list):
 
     def __init__(self, size=3):
         self.size = size
         super(TabuList, self).__init__()
 
-    # dodavanje stanja u listu FIFO naèinom
+    # dodavanje stanja u listu FIFO nacinom
     def append(self, element):
         if len(self) == self.size:          
             self.pop(0)
             return super(TabuList, self).append(element)
         return super(TabuList, self).append(element)
 
-    # provjera sadrži li tabu lista dano stanje
+    # provjera sadrzi li tabu lista dano stanje
     def __contains__(self, move):
         for i in range(len(self)):
             if move == self[i]:
