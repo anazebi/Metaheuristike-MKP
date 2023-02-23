5957ff0ee5457a3e4af1b7e7992235598cc3e327
elekova <125138212+elekova@users.noreply.github.com>
2023-02-23 01:28:09 +0100

    Update Knapsack.py

--- Knapsack.py
+++ Knapsack.py
@@ -49,7 +49,7 @@ class Ruksak(object):
     def sortirani(self,predmeti, key = Item.ratio):
         return sorted(predmeti, key = key, reverse = True)
     
-    def remove_predmet(self, predmet):
+    def makni_predmet(self, predmet):
         if predmet in self.predmeti:
             for i in range(properties_number):
                 setattr(self, 'property{}'.format(i+1), getattr(self, 'property{}'.format(i+1))+ getattr(predmet, 'property{}'.format(i+1)))
@@ -59,6 +59,69 @@ class Ruksak(object):
             self.svi_predmeti.append(predmet)
             return True
         return False
+   
+    def izvsi_korak(self, korak, silent=False):
+        for predmet in korak.makni_predmete:
+            if not predmet in self:
+                return False
+            self.makni_predmet(predmet)
+        for predmet in korak.dodaj_predmete:
+            if not self.mogu_dodati(predmet):
+                return False
+            self.dodaj_predmet(predmet)
+        if not silent:
+            self.broj_iteracija += 1
+            self.koraci_do_sada.append(korak)
+            
+    def evaluiraj_zamjenu(self, predmet1, predmet2):
+        return self.mogu_zamjeniti(predmet1, predmet2)
+    
+    
+    def mogu_zamjeniti(self, predmet1, predmet2):
+        if predmet1 not in self.predmeti or predmet2 in self.predmeti:
+            return False
+        for i in range properties_number:
+            if not (getattr(predmet2, 'con{}'.format(i)) <= (getattr(self, 'con{}'.format(i))+getattr(predmet1, 'con{}'.format(i)))
+                    return False
+        return self.vrijednost - predmet1.vrijednost + predmet2.vrijednost
+                    
+    def zamjena(predmet1, predmet2):
+        if self.mogu_zamjeniti(predmet1, predmet2):
+             self.makni_predmet(predmet1)
+             self.dodaj_predmet(predmet2)
+             return True
+        return False
+
+
     
     def __contains__(self, predmet):
         return any(map(lambda x: x == predmet, self.predmeti))
+     
+ class Korak(object):
+
+    def __init__(self, dodaj_predmete=[], makni_predmete=[]):
+        self.dodaj_predmete = dodaj_predmete
+        self.makni_predmete = makni_predmete
+
+    @property #ne kuzim to?
+
+    def evaluiraj_korak(self):
+        makni_vrijednost = dodaj_vrijednost = 0
+        if not len(self.makni_predmete) == 0:
+            makni_vrijednost = reduce(lambda x, y: x+y, [predmet.vrijednost for predmet in self.makni_predmete])
+        if not len(self.dodaj_predmete) == 0:
+            dodaj_vrijednost = reduce(lambda x, y: x+y, [predmet.vrijednost for predmet in self.dodaj_predmete])
+        return dodaj_vrijednost - makni_vrijednost
+
+    def okreni(self):
+        return Korak( dodaj_predmete=self.makni_predmete, makni_predmete=self.dodaj_predmete)
+    
+    def __eq__(self, korak1):
+        if not isinstance(korak1, Korak):
+            return False
+        return self.dodaj_predmete == korak1.dodaj_predmete and self.makni_predmete == korak1.makni_predmete
+                    
+                    
+
+                    
+               
